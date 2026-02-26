b826e6e7616aa5bf08b2b686bfb4046d11b393bc
משתמש זה < @DESKTOP-SN2JHFI>
2026-02-26 14:28:16 +0200

    Add Customer model

---
+++ CarRental/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRental.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string LicenseNumber { get; set; }
+
+        //public virtual ICollection<Rental> Rentals { get; set; }
+    }
+}
